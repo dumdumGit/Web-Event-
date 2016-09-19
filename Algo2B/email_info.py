14217753bafcb03f605d0a7e49b0b7605b37506f
dumdumGit <urang.sadan@gmail.com>
2016-09-20 00:11:50 +0700

    Update email_info.py

--- Algo2B/email_info.py
+++ Algo2B/email_info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'muhamad.sadan@gmail.com'
-EMAIL_HOST_PASSWORD = 'blogsaya'
-EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
+EMAIL_PORT = 
